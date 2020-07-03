5928239d92c3f9eab1c4ebc15e173e3454f300aa
Gideon <GideonEsquire@gmail.com>
2020-07-03 12:35:29 -0700

    better styling and simplifying

--- sketch.js
+++ sketch.js
@@ -1,33 +1,22 @@
 function setup() {
-  createCanvas(0);
+  createCanvas(0, 0);
   let lines = 0;
-  let lines_button;
-  let lines_element;
-
   let shorts = 0;
-  let shorts_button;
-  let shorts_element;
-
   let orders = 0;
-  let order_button;
-  let order_element;
-
   let lates = 0;
-  let lates_button;
-  let lates_element;
 
   lines_element = createElement('span', lines).addClass('b');
   shorts_element = createElement('span', shorts).addClass('b');
   lates_element = createElement('span', lates).addClass('b');
   order_element = createElement('span', orders).addClass('b');
 
-  lines_button = createButton('Increase').addClass('mainClicker').mousePressed(() => lines_element.html(++lines));
-  shorts_button = createButton('Increase').addClass('mainClicker').mousePressed(() => shorts_element.html(++shorts));
-  lates_button = createButton('Increase').addClass('mainClicker').mousePressed(() => lates_element.html(++lates));
-  order_button = createButton('Increase').addClass('mainClicker').mousePressed(() => order_element.html(++orders));
+  lines_button = createButton('+').addClass('mainClicker').mousePressed(() => lines_element.html(++lines));
+  shorts_button = createButton('+').addClass('mainClicker').mousePressed(() => shorts_element.html(++shorts));
+  lates_button = createButton('+').addClass('mainClicker').mousePressed(() => lates_element.html(++lates));
+  order_button = createButton('+').addClass('mainClicker').mousePressed(() => order_element.html(++orders));
 
-  lines_button_minus = createButton('Decrease').addClass('mainClicker').mousePressed(() => lines_element.html(--lines));
-  shorts_button_minus = createButton('Decrease').addClass('mainClicker').mousePressed(() => shorts_element.html(--shorts));
-  lates_button_minus = createButton('Decrease').addClass('mainClicker').mousePressed(() => -lates_element.html(--lates));
-  order_button_minus = createButton('Decrease').addClass('mainClicker').mousePressed(() => order_element.html(--orders));
+  lines_button_minus = createButton('-').addClass('mainClicker').mousePressed(() => lines_element.html(--lines));
+  shorts_button_minus = createButton('-').addClass('mainClicker').mousePressed(() => shorts_element.html(--shorts));
+  lates_button_minus = createButton('-').addClass('mainClicker').mousePressed(() => -lates_element.html(--lates));
+  order_button_minus = createButton('-').addClass('mainClicker').mousePressed(() => order_element.html(--orders));
 }
